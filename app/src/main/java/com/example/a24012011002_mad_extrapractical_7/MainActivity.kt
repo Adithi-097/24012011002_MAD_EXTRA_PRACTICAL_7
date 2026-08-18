6d243a375da93262bab9659abbd07fe3a480547c
Adithi-097 <ads83471652@gmail.com>
2026-08-18 22:52:26 +0530

    Add play and stop button functionality

--- app/src/main/java/com/example/a24012011002_mad_extrapractical_7/MainActivity.kt
+++ app/src/main/java/com/example/a24012011002_mad_extrapractical_7/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.a24012011002_mad_extrapractical_7
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +17,16 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        findViewById<FloatingActionButton>(R.id.play_button).setOnClickListener {
+            Intent(applicationContext, MusicService::class.java).
+            putExtra(MusicService.SERVICE_KEY,
+                MusicService.SERVICE_DATA).also { startService(it) }
+
+        }
+        findViewById<FloatingActionButton>(R.id.stop_button).setOnClickListener {
+            Intent(applicationContext, MusicService::class.java).also { stopService(it) }
         }
     }
 }
